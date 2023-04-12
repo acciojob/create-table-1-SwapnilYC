df72f45921017ced94265d9667292133ef7dc3e4
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-12 18:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let table = document.getElementById('sampleTable');
+let i = 0;
 function insert_Row() {
     //Write your code here
   let html = `
   <tr>
-  <td>New Cell1</td>
-  <td>New Cell2</td>
+  <td>New Cell${++i}</td>
+  <td>New Cell${++i}</td>
   </tr> 
   `
   table.insertAdjacentHTML('afterbegin', html);
