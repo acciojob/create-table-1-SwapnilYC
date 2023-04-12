884c5a2680deab414df288921d5097893b63f973
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-12 18:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let table = document.getElementById('sampleTable');
 function insert_Row() {
     //Write your code here
-  
-  
+  let html = `
+  <tr>
+  <td>New Cell1</td>
+  <td>new Cell2</td>
+  </tr> 
+  `
+  table.insertAdjacentHTML('afterbegin', html);
 }
