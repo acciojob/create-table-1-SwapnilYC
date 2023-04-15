f33aec70396c12ec4e69719c913ae6d8541283d0
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-15 13:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let table = document.getElementById('sampleTable');
+// let table = document.getElementById('sampleTable');
+// // let i = 0;
+// function insert_Row() {
+//     //Write your code here
+// let i = 0;
+//   let html = `
+//   <tr>
+//   <td>New Cell${++i}</td>
+//   <td>New Cell${++i}</td>
+//   </tr> 
+//   `
+//   table.insertAdjacentHTML('afterbegin', html);
+// }
+
 function insert_Row() {
-    //Write your code here
-let i = 0;
-  let html = `
-  <tr>
-  <td>New Cell${++i}</td>
-  <td>New Cell${++i}</td>
-  </tr> 
-  `
-  table.insertAdjacentHTML('afterbegin', html);
+let table = document.getElementById('sampleTable');
+let newRow = table.insertRow(0);
+let newCell1 = newRow.insertCell(0);
+let newCell2 = newRow.insertCell(1);
+newCell1.innerHTML = 'New Cell 1';
+newCell2.innerHTML = 'New Cell 2';
 }
